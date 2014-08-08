ecf483079869bef76a0c52609c162ba8fcbba44d
Chris Oates <chris_oates@hotmail.com>
2014-08-08 13:44:47 -0700

    Testing

--- Make-Solution.bat
+++ Make-Solution.bat
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -DZLIB_LIBRARY=%ZLIB_LIBRARY% -DZLIB_INCLUDE_DIR=%ZLIB_INCLUDE_DIR% -G "Visual Studio 12"
+cmake .. -DZLIB_LIBRARY=%ZLIB_LIBRARY% -DZLIB_INCLUDE_DIR=%ZLIB_INCLUDE_DIR% -G "Visual Studio 10"
